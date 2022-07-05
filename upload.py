06985cce3a2692c19ef97e589535eb2dfc5a023a
TheCodingStudent <a360069@uach.mx>
2022-07-04 23:15:50 -0600

    uploader class

--- upload.py
+++ upload.py
@@ -1,6 +1,6 @@
 """
 This is a simple tool to upload the Pylejandria package to Pypi and GitHub with
-a UI
+a UI. Version 1.0.3 By Armando Chaparro.
 """
 
 import os
@@ -10,131 +10,153 @@ import tkinter as tk
 from tkinter.filedialog import askdirectory
 from tkinter import ttk
 
-REGEX = '[0-9]+\.[0-9]+\.[0-9]+'
-PATH = os.getcwd()
 
-with open(os.path.join(PATH, 'setup.cfg'), 'r') as f:
-    text = f.read()
-    old_version = re.search(REGEX, text).group()
-    match old_version.split('.'):
-        case(x, y, z):
-            VERSION = f'{x}.{y}.{int(z) + 1}'
+class Uploader:
+    def __init__(self) -> None:
+        self.regex = '[0-9]+\.[0-9]+\.[0-9]+'
+        self.path = os.getcwd()
+        self.get_version()
+        self.commit = f'version {self.version}'
 
+    def get_version(self) -> None:
+        with open(os.path.join(self.path, 'setup.cfg'), 'r') as f:
+            self.text = f.read()
+            self.old_version = re.search(self.regex, self.text).group()
+            match self.old_version.split('.'):
+                case(x, y, z):
+                    self.version = f'{x}.{y}.{int(z) + 1}'
 
-COMMIT = f'version {VERSION}'
+    def validate_version(self) -> None:
+        """
+        Checks if the current version is valid, it must follow the pattern:
+        x.x.x, if it doesnt then is deleted to prevent any error. 
+        """
+        text = self.version_entry.get()
+        if re.match(f'^{self.regex}$', text):
+            self.version_status['fg'] = '#00ff00'
+            self.version_status['text'] = 'Valid version'
+        else:
+            self.version_status['fg'] = '#ff0000'
+            self.version_status['text'] = 'Invalid version'
+            self.version_entry.delete(0, tk.END)
 
+    def upload(self) -> None:
+        """
+        Based on the PYPI and GITHUB variables, uploads to their respective sites
+        using the same commands that are used in terminal.
+        """
+        if self.pypi is True:
+            with open(os.path.join(self.path, 'setup.cfg'), 'w') as f:
+                f.write(self.text.replace(self.old_version, self.version))
 
-def validate_version():
-    text = version_entry.get()
-    if re.match(f'^{REGEX}$', text):
-        version_status['fg'] = '#00ff00'
-        version_status['text'] = 'Valid version'
-    else:
-        version_status['fg'] = '#ff0000'
-        version_status['text'] = 'Invalid version'
-        version_entry.delete(0, tk.END)
+            os.system('python -m build')
+            file1 = os.path.join(
+                self.path, f'dist/pylejandria-{self.version}-py3-none-any.whl'
+            )
+            file2 = os.path.join(self.path, f'dist/pylejandria-{self.version}.tar.gz')
+            os.system(f'twine upload {file1} {file2}')
+            print(f'{10*"-"}uploaded to Pypi{10*"-"}')
 
+        if self.github is True:
+            os.system('git add .')
+            os.system(f'git commit -m "{self.commit}"')
+            os.system('git push')
+            print(f'{10*"-"}uploaded to GitHub{10*"-"}')
 
-def upload():
-    if PYPI is True:
-        with open(os.path.join(PATH, 'setup.cfg'), 'w') as f:
-            f.write(text.replace(old_version, VERSION))
+    def get_values(self) -> None:
+        """
+        Updates all global variables and starts a new thread to run the uploading,
+        the thread is necessary to run in parallel with the UI.
+        """
+        self.commit = self.commit_entry.get()
+        self.version = self.version_entry.get()
+        self.github = self.git_combobox.current() == 0
+        self.pypi = self.pypi_combobox.current() == 0
+        thread = threading.Thread(target=self.upload)
+        thread.start()
 
-        os.system('python -m build')
-        file1 = os.path.join(
-            PATH, f'dist/pylejandria-{VERSION}-py3-none-any.whl'
-        )
-        file2 = os.path.join(PATH, f'dist/pylejandria-{VERSION}.tar.gz')
-        os.system(f'twine upload {file1} {file2}')
-        print(f'{10*"-"}uploaded to Pypi{10*"-"}')
+    def change_path(self) -> None:
+        """
+        Updates the path to upload from.
+        """
+        tk.Tk().withdraw()
+        self.path = askdirectory()
+        self.path_entry.delete(0, tk.END)
+        self.path_entry.insert(0, self.path)
 
-    if GITHUB is True:
-        os.system('git add .')
-        os.system(f'git commit -m "{COMMIT}"')
-        os.system('git push')
-        print(f'{10*"-"}uploaded to GitHub{10*"-"}')
+    def update_git(self, event: tk.Event) -> None:
+        """
+        Updates all the widgets related to git configuration.
+        """
+        if self.git_combobox.current():
+            self.commit_entry['state'] = 'disabled'
+        else:
+            self.commit_entry['state'] = 'normal'
 
+    def update_pypi(self, event: tk.Event) -> None:
+        """
+        Updates all the widgets related to Pypi configuration.
+        """
+        if self.pypi_combobox.current():
+            self.version_entry['state'] = 'disabled'
+            self.commit_entry.delete(0, tk.END)
+            self.commit_entry.insert(0, '')
+        else:
+            self.version_entry['state'] = 'normal'
+            self.commit_entry.delete(0, tk.END)
+            self.commit_entry.insert(0, self.commit)
 
-def get_values():
-    global COMMIT, VERSION, GITHUB, PYPI
-    COMMIT = commit_entry.get()
-    VERSION = version_entry.get()
-    GITHUB = git_combobox.current() == 0
-    PYPI = pypi_combobox.current() == 0
-    thread = threading.Thread(target=upload)
-    thread.start()
+    def run(self) -> None:
+        root = tk.Tk()
+        root.title('Uploader By Armando Chaparro')
 
+        path_label = tk.Label(root, text='Folder')
+        path_label.grid(row=0, column=0, padx=5, sticky='w')
+        self.path_entry = tk.Entry(root, width=50)
+        self.path_entry.insert(0, self.path)
+        self.path_entry.grid(row=0, column=1, padx=5, sticky='w')
+        path_button = tk.Button(root, text='Change', command=self.change_path)
+        path_button.grid(row=0, column=2, padx=5, sticky='w')
 
-def change_path():
-    global PATH
-    tk.Tk().withdraw()
-    PATH = askdirectory()
-    path_entry.delete(0, tk.END)
-    path_entry.insert(0, PATH)
+        version_label = tk.Label(root, text='Version')
+        version_label.grid(row=1, column=0, padx=5, sticky='w')
+        self.version_entry = tk.Entry(root, width=15)
+        self.version_entry.insert(0, self.version)
+        self.version_entry.grid(row=1, column=1, padx=5, sticky='w')
+        version_button = tk.Button(root, text='Validate', command=self.validate_version)
+        version_button.grid(row=1, column=2, padx=5)
+        self.version_status = tk.Label(root, text='')
+        self.version_status.grid(row=1, column=3, padx=5)
 
+        git_label = tk.Label(root, text='Upload to GIT')
+        git_label.grid(row=2, column=0, padx=5, sticky='w')
+        self.git_combobox = ttk.Combobox(root, width=15)
+        self.git_combobox['values'] = ['True', 'False']
+        self.git_combobox['state'] = 'readonly'
+        self.git_combobox.bind("<<ComboboxSelected>>", self.update_git)
+        self.git_combobox.current(0)
+        self.git_combobox.grid(row=2, column=1, padx=5, sticky='w')
 
-def update_git(event):
-    if git_combobox.current():
-        commit_entry['state'] = 'disabled'
-    else:
-        commit_entry['state'] = 'normal'
+        commit_label = tk.Label(root, text='Commit')
+        commit_label.grid(row=3, column=0, padx=5, sticky='w')
+        self.commit_entry = tk.Entry(root, width=30)
+        self.commit_entry.insert(0, self.commit)
+        self.commit_entry.grid(row=3, column=1, padx=5, sticky='w')
 
-def update_pypi(event):
-    if pypi_combobox.current():
-        version_entry['state'] = 'disabled'
-        commit_entry.delete(0, tk.END)
-        commit_entry.insert(0, '')
-    else:
-        version_entry['state'] = 'normal'
-        commit_entry.delete(0, tk.END)
-        commit_entry.insert(0, COMMIT)
+        pypi_label = tk.Label(root, text='Upload to Pypi')
+        pypi_label.grid(row=4, column=0, padx=5, sticky='w')
+        self.pypi_combobox = ttk.Combobox(root, width=15)
+        self.pypi_combobox.bind("<<ComboboxSelected>>", self.update_pypi)
+        self.pypi_combobox['values'] = ['True', 'False']
+        self.pypi_combobox['state'] = 'readonly'
+        self.pypi_combobox.current(0)
+        self.pypi_combobox.grid(row=4, column=1, padx=5, sticky='w')
 
-root = tk.Tk()
-root.title('Uploader By Armando Chaparro')
+        upload_button = tk.Button(root, text='Upload', command=self.get_values)
+        upload_button.grid(row=5, column=1, padx=5, pady=10, sticky='ew')
 
-path_label = tk.Label(root, text='Folder')
-path_label.grid(row=0, column=0, padx=5, sticky='w')
-path_entry = tk.Entry(root, width=50)
-path_entry.insert(0, PATH)
-path_entry.grid(row=0, column=1, padx=5, sticky='w')
-path_button = tk.Button(root, text='Change', command=change_path)
-path_button.grid(row=0, column=2, padx=5, sticky='w')
+        root.mainloop()
 
-version_label = tk.Label(root, text='Version')
-version_label.grid(row=1, column=0, padx=5, sticky='w')
-version_entry = tk.Entry(root, width=15)
-version_entry.insert(0, VERSION)
-version_entry.grid(row=1, column=1, padx=5, sticky='w')
-version_button = tk.Button(root, text='Validate', command=validate_version)
-version_button.grid(row=1, column=2, padx=5)
-version_status = tk.Label(root, text='')
-version_status.grid(row=1, column=3, padx=5)
-
-git_label = tk.Label(root, text='Upload to GIT')
-git_label.grid(row=2, column=0, padx=5, sticky='w')
-git_combobox = ttk.Combobox(root, width=15)
-git_combobox['values'] = ['True', 'False']
-git_combobox['state'] = 'readonly'
-git_combobox.bind("<<ComboboxSelected>>", update_git)
-git_combobox.current(0)
-git_combobox.grid(row=2, column=1, padx=5, sticky='w')
-
-commit_label = tk.Label(root, text='Commit')
-commit_label.grid(row=3, column=0, padx=5, sticky='w')
-commit_entry = tk.Entry(root, width=30)
-commit_entry.insert(0, COMMIT)
-commit_entry.grid(row=3, column=1, padx=5, sticky='w')
-
-pypi_label = tk.Label(root, text='Upload to Pypi')
-pypi_label.grid(row=4, column=0, padx=5, sticky='w')
-pypi_combobox = ttk.Combobox(root, width=15)
-pypi_combobox.bind("<<ComboboxSelected>>", update_pypi)
-pypi_combobox['values'] = ['True', 'False']
-pypi_combobox['state'] = 'readonly'
-pypi_combobox.current(0)
-pypi_combobox.grid(row=4, column=1, padx=5, sticky='w')
-
-upload_button = tk.Button(root, text='Upload', command=get_values)
-upload_button.grid(row=5, column=1, padx=5, pady=10, sticky='ew')
-
-root.mainloop()
+if __name__ == '__main__':
+    app = Uploader()
+    app.run()
